79fd3d9af22da9d21b73fd28307d50888ea9eaee
a4refillpad <github@man.lc>
2017-03-23 23:38:19 +0000

    added new colours

--- devicetypes/a4refillpad/xiaomi-zigbee-outlet.src/xiaomi-zigbee-outlet.groovy
+++ devicetypes/a4refillpad/xiaomi-zigbee-outlet.src/xiaomi-zigbee-outlet.groovy
@@ -39,7 +39,7 @@ metadata {
 
     tiles(scale: 2) {
         multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") { 
                 attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
                 attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
                 attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
