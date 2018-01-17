25c88b9b5892b149c598a84f8b22ae8de29aad16
ronvandegraaf <ron.van.de.graaf@gmail.com>
2018-01-17 08:49:25 +0100

    Changed colors for dry & wet state
    Changed to colors to dry (=blue) and wet (=orange), like the door/window sensors. To give more attention in your 'Things' list.

--- devicetypes/bspranger/xiaomi-aqara-leak-sensor.src/xiaomi-aqara-leak-sensor.groovy
+++ devicetypes/bspranger/xiaomi-aqara-leak-sensor.src/xiaomi-aqara-leak-sensor.groovy
@@ -64,8 +64,8 @@ metadata {
     tiles(scale: 2) {
         multiAttributeTile(name:"water", type: "generic", width: 6, height: 4){
             tileAttribute ("device.water", key: "PRIMARY_CONTROL") {
-                attributeState "dry", label:"Dry", icon:"st.alarm.water.dry", backgroundColor:"#ffffff"
-                attributeState "wet", label:"Wet", icon:"st.alarm.water.wet", backgroundColor:"#00a0dc"
+                attributeState "dry", label:"Dry", icon:"st.alarm.water.dry", backgroundColor:"#00a0dc"
+                attributeState "wet", label:"Wet", icon:"st.alarm.water.wet", backgroundColor:"#e86d13"
             }
         }
         valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
