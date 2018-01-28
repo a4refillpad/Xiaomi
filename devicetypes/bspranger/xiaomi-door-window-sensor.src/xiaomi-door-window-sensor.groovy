1b27cdcb70cf48c3920da5edb0fb23f1112afbe7
Steven Dale <dale.steven@gmail.com>
2018-01-28 13:49:34 +0000

    Remove Auto Battery Code

--- devicetypes/bspranger/xiaomi-door-window-sensor.src/xiaomi-door-window-sensor.groovy
+++ devicetypes/bspranger/xiaomi-door-window-sensor.src/xiaomi-door-window-sensor.groovy
@@ -180,11 +180,6 @@ private Map getBatteryResult(rawValue) {
     ]
     
     log.debug "${device.displayName}: ${result}"
-    if (state.battery != result.value)
-    {
-    	state.battery = result.value
-        resetBatteryRuntime()
-    }
     return result
 }
 
