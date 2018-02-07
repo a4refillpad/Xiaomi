c1ef8959af53c1d7d35aa6287f5d63e08938f518
Steven Dale <dale.steven@gmail.com>
2018-02-07 12:27:53 +0000

    Error

--- devicetypes/bspranger/xiaomi-aqara-button.src/xiaomi-aqara-button.groovy
+++ devicetypes/bspranger/xiaomi-aqara-button.src/xiaomi-aqara-button.groovy
@@ -115,6 +115,7 @@ metadata {
 		input name: "voltsmax", title: "Max Volts\nA battery is at 100% at __ volts\nRange 2.8 to 3.4", type: "decimal", range: "2.8..3.4", defaultValue: 3, required: false
 		input name: "voltsmin", title: "Min Volts\nA battery is at 0% (needs replacing) at __ volts\nRange 2.0 to 2.7", type: "decimal", range: "2..2.7", defaultValue: 2.5, required: false
 		}
+     }
 }
 
 //adds functionality to press the centre tile as a virtualApp Button
