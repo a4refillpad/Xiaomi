c37fcd92bb085acbefa85e161351f7ac21f9b611
Steven Dale <dale.steven@gmail.com>
2018-02-07 12:31:23 +0000

    Standardisation

--- devicetypes/bspranger/xiaomi-button.src/xiaomi-button.groovy
+++ devicetypes/bspranger/xiaomi-button.src/xiaomi-button.groovy
@@ -40,18 +40,7 @@
  *        deviceJoinName: whatever you want it to show in the app as a Thing
  *
  */
-preferences {
-	input ("holdTime", "number", title: "Minimum time in seconds for a press to count as \"held\"", defaultValue: 4, displayDuringSetup: false)
-        input name: "PressType", type: "enum", options: ["Toggle", "Momentary"], description: "Effects how the button toggles", defaultValue: "Toggle", displayDuringSetup: true
-    	input name: "dateformat", type: "enum", title: "Set Date Format\n US (MDY) - UK (DMY) - Other (YMD)", description: "Date Format", required: false, options:["US","UK","Other"]
-	input name: "clockformat", type: "bool", title: "Use 24 hour clock?", defaultValue: false, required: false
-	input description: "Only change the settings below if you know what you're doing", displayDuringSetup: false, type: "paragraph", element: "paragraph", title: "ADVANCED SETTINGS"
-	input name: "voltsmax", title: "Max Volts\nA battery is at 100% at __ volts\nRange 2.8 to 3.4", type: "decimal", range: "2.8..3.4", defaultValue: 3, required: false
-	input name: "voltsmin", title: "Min Volts\nA battery is at 0% (needs replacing) at __ volts\nRange 2.0 to 2.7", type: "decimal", range: "2..2.7", defaultValue: 2.5, required: false
-	input description: "Changed your battery? Reset the date", displayDuringSetup: false, type: "paragraph", element: "paragraph", title: "Battery Changed"
-	input name: "battReset", type: "bool", title: "Battery Changed?", description: "", displayDuringSetup: false
-} 
- 
+
 metadata {
     definition (name: "Xiaomi Button", namespace: "bspranger", author: "bspranger") {
         capability "Battery"
@@ -61,11 +50,11 @@ metadata {
 	capability "Health Check"
         capability "Momentary"
         
+        attribute "lastCheckin", "string"
+        attribute "lastCheckinDate", "Date"
         attribute "lastpressed", "string"
         attribute "lastpressedDate", "string"
         attribute "batterylevel", "string"
-        attribute "lastCheckin", "string"
-        attribute "lastCheckinDate", "Date"
         attribute "batteryRuntime", "String"
 
         fingerprint endpointId: "01", profileId: "0104", deviceId: "0104", inClusters: "0000,0003,FFFF,0019", outClusters: "0000,0004,0003,0006,0008,0005,0019", manufacturer: "LUMI", model: "lumi.sensor_switch", deviceJoinName: "Original Xiaomi Button"
@@ -98,7 +87,7 @@ metadata {
 			]
         }
         valueTile("lastcheckin", "device.lastCheckin", decoration: "flat", inactiveLabel: false, width: 4, height: 1) {
-            state "default", label:'Last Checkin:\n${currentValue}'
+            state "default", label:'Last Event:\n${currentValue}'
         }
 	valueTile("batteryRuntime", "device.batteryRuntime", inactiveLabel: false, decoration: "flat", width: 4, height: 1) {
 	    state "batteryRuntime", label:'Battery Changed: ${currentValue}'
@@ -106,6 +95,26 @@ metadata {
         main (["button"])
         details(["button","battery","lastcheckin","batteryRuntime"])
    }
+   preferences {
+	section {	
+		input name: "holdTime", "number", title: "Minimum time in seconds for a press to count as \"held\"", defaultValue: 4
+        	input name: "PressType", type: "enum", options: ["Toggle", "Momentary"], description: "Effects how the button toggles", defaultValue: "Toggle"
+		}
+	section {
+		input description: "", type: "paragraph", element: "paragraph", title: "DATE & CLOCK"    
+		input name: "dateformat", type: "enum", title: "Set Date Format\n US (MDY) - UK (DMY) - Other (YMD)", description: "Date Format", options:["US","UK","Other"]
+		input name: "clockformat", type: "bool", title: "Use 24 hour clock?", defaultValue: false
+		}
+	section {
+            	input description: "If you have installed a new battery, the toggle below will reset the Changed Battery date to help remember when it was changed.", type: "paragraph", element: "paragraph", title: "CHANGED BATTERY DATE RESET"
+		input name: "battReset", type: "bool", title: "Battery Changed?", description: ""
+		}
+	section {
+            	input description: "Only change the settings below if you know what you're doing.", type: "paragraph", element: "paragraph", title: "ADVANCED SETTINGS"
+		input name: "voltsmax", title: "Max Volts\nA battery is at 100% at __ volts\nRange 2.8 to 3.4", type: "decimal", range: "2.8..3.4", defaultValue: 3, required: false
+		input name: "voltsmin", title: "Min Volts\nA battery is at 0% (needs replacing) at __ volts\nRange 2.0 to 2.7", type: "decimal", range: "2..2.7", defaultValue: 2.5, required: false
+		}
+     }
 }
 
 //adds functionality to press the centre tile as a virtualApp Button
@@ -125,7 +134,7 @@ def parse(String description) {
     //  send event for heartbeat    
     def now = formatDate()	
     def nowDate = new Date(now).getTime()
-    sendEvent(name: "lastCheckin", value: now)
+    sendEvent(name: "lastCheckin", value: now, displayed: false)
     sendEvent(name: "lastCheckinDate", value: nowDate, displayed: false) 
 
     Map map = [:]
