8ac6f41978ee334644ed6b6e225f27d83118b877
Keith G <33558908+veeceeoh@users.noreply.github.com>
2018-03-22 12:20:50 -0700

    added info/debug log display settings
    Changes / Fixes
    • added logging message display preference settings, with separate toggles for info message output (defaults to ON) and debug message output (defaults to OFF)
    • Cleaned up log messages for consistency and assigned them into info / debug groups to work with new logging preference settings
    • re-added `capability "Momentary"` to be used by the virtual button tile.
    • reworked the main tile and push() command code so that pushing the virtual button simulates a single click of the Xiaomi button with a button `pushed` event and the accompanying custom `lastPressed/CoRE` and `lastReleased/CoRE` events - but because button "released" events are not supported, there is no way for the tile to give visual feedback on the press - just look at the events log or logging window if info logging is turned on
    • added a failsafe to the "wait until held" calculation to avoid false positives on button "held" - if the calculated difference between button messages more than 10 seconds over the "wait until held" time setting, then a button `pushed` event is sent instead of `held`
    • edited some comments for clarity

--- devicetypes/bspranger/xiaomi-button.src/xiaomi-button.groovy
+++ devicetypes/bspranger/xiaomi-button.src/xiaomi-button.groovy
@@ -1,24 +1,24 @@
 /**
- *  Xiaomi Zigbee Button
- *  Version 1.2
+ *	Xiaomi Zigbee Button
+ *	Version 1.2
  *
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *	in compliance with the License. You may obtain a copy of the License at:
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *	for the specific language governing permissions and limitations under the License.
  *
- *  Original device handler code by a4refillpad, adapted for use with Aqara model by bspranger
- *  Additional contributions to code by alecm, alixjg, bspranger, gn0st1c, foz333, jmagnuson, rinkek, ronvandegraaf, snalee, tmleafs, twonk, & veeceeoh
+ *	Original device handler code by a4refillpad, adapted for use with Aqara model by bspranger
+ *	Additional contributions to code by alecm, alixjg, bspranger, gn0st1c, foz333, jmagnuson, rinkek, ronvandegraaf, snalee, tmleafs, twonk, & veeceeoh
  *
- *  Known issues:
- *  Xiaomi sensors do not seem to respond to refresh requests
- *  Inconsistent rendering of user interface text/graphics between iOS and Android devices - This is due to SmartThings, not this device handler
- *  Pairing Xiaomi sensors can be difficult as they were not designed to use with a SmartThings hub.
+ *	Known issues:
+ *	Xiaomi sensors do not seem to respond to refresh requests
+ *	Inconsistent rendering of user interface text/graphics between iOS and Android devices - This is due to SmartThings, not this device handler
+ *	Pairing Xiaomi sensors can be difficult as they were not designed to use with a SmartThings hub.
  *
  *
  */
@@ -30,6 +30,7 @@ metadata {
 		capability "Button"
 		capability "Holdable Button"
 		capability "Actuator"
+		capability "Momentary"
 		capability "Configuration"
 		capability "Health Check"
 
@@ -48,18 +49,18 @@ metadata {
 	}
 
 	simulator {
-		status "Pressed": "on/off: 0"
-		status "Released": "on/off: 1"
+		status "Press button": "on/off: 0"
+		status "Release button": "on/off: 1"
 	}
 
 	tiles(scale: 2) {
 		multiAttributeTile(name:"button", type: "lighting", width: 6, height: 4, canChangeIcon: true) {
 			tileAttribute ("device.button", key: "PRIMARY_CONTROL") {
 				attributeState("pushed", label:'Pushed', action: "momentary.push", backgroundColor:"#00a0dc")
-				attributeState("held", label:'Held', action: "momentary.push", backgroundColor:"#00a0dc")
+				attributeState("held", label:'Held', backgroundColor:"#00a0dc")
 			}
-			tileAttribute("device.lastpressed", key: "SECONDARY_CONTROL") {
-				attributeState "default", label:'Last Pressed: ${currentValue}'
+			tileAttribute("device.lastPressed", key: "SECONDARY_CONTROL") {
+				attributeState "lastPressed", label:'Last Pressed: ${currentValue}'
 			}
 		}
 		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
@@ -82,14 +83,19 @@ metadata {
 
 	preferences {
 		//Button Config
+		input description: "", type: "paragraph", element: "paragraph", title: "BUTTON CONFIGURATION"
 		input name: "waittoHeld", type: "decimal", title: "If the button is held, wait how many seconds until sending a 'held' message?", description: "Number of seconds (default = 2.0)", range: "0.1..120"
 		//Date & Time Config
 		input description: "", type: "paragraph", element: "paragraph", title: "DATE & CLOCK"
-		input name: "dateformat", type: "enum", title: "Set Date Format\n US (MDY) - UK (DMY) - Other (YMD)", description: "Date Format", options:["US","UK","Other"]
+		input name: "dateformat", type: "enum", title: "Set Date Format\nUS (MDY) - UK (DMY) - Other (YMD)", description: "Date Format", options:["US","UK","Other"]
 		input name: "clockformat", type: "bool", title: "Use 24 hour clock?"
 		//Battery Reset Config
 		input description: "If you have installed a new battery, the toggle below will reset the Changed Battery date to help remember when it was changed.", type: "paragraph", element: "paragraph", title: "CHANGED BATTERY DATE RESET"
 		input name: "battReset", type: "bool", title: "Battery Changed?"
+		//Live Logging Message Display Config
+		input description: "These settings affect the display of messages in the Live Logging tab of the SmartThings IDE.", type: "paragraph", element: "paragraph", title: "LIVE LOGGING"
+		input name: "infoLogging", type: "bool", title: "Display info log messages?", defaultValue: true
+		input name: "debugLogging", type: "bool", title: "Display debug log messages?"
 		//Battery Voltage Offset
 		input description: "Only change the settings below if you know what you're doing.", type: "paragraph", element: "paragraph", title: "ADVANCED SETTINGS"
 		input name: "voltsmax", type: "decimal", title: "Max Volts\nA battery is at 100% at __ volts\nRange 2.8 to 3.4", range: "2.8..3.4", defaultValue: 3, required: false
@@ -99,7 +105,7 @@ metadata {
 
 //adds functionality to press the centre tile as a virtualApp Button
 def push() {
-	log.debug "Virtual App Button Pressed"
+	displayInfoLog(": Virtual App Button Pressed")
 	sendEvent(name: "lastPressed", value: formatDate(), displayed: false)
 	sendEvent(name: "lastPressedCoRE", value: now(), displayed: false)
 	sendEvent(name: "button", value: "pushed", data: [buttonNumber: 1], descriptionText: "$device.displayName app button was pushed", isStateChange: true)
@@ -109,7 +115,7 @@ def push() {
 
 // Parse incoming device messages to generate events
 def parse(String description) {
-	log.debug "${device.displayName}: Parsing '${description}'"
+	displayDebugLog(": Parsing '${description}'")
 	def result = [:]
 
 	// Any report - button press & Battery - results in a lastCheckin event and update to Last Checkin tile
@@ -124,8 +130,11 @@ def parse(String description) {
 	} else if (description?.startsWith("read attr - raw: ")) {
 		result = parseReadAttrMessage(description)
 	}
-	log.debug "${device.displayName}: Parse returned $result"
-	return createEvent(result)
+	if (result != [:]) {
+		displayDebugLog(": Creating event $result")
+		return createEvent(result)
+	} else
+		return [:]
 }
 
 private parseButtonMessage(description) {
@@ -134,13 +143,13 @@ private parseButtonMessage(description) {
 
 	if (onOff == '0') {
 		// on button pressed update lastPressed and lastButtonMssg to current date/time
-		log.debug "${device.displayName}: Button pressed, setting Last Pressed to current date/time"
+		displayDebugLog(": Button pressed, setting lastPressed to current date/time")
 		sendEvent(name: "lastPressed", value: formatDate(), displayed: false)
 		sendEvent(name: "lastPressedCoRE", value: now(), displayed: false)
 		sendEvent(name: "lastButtonMssg", value: now(), displayed: false)
 	} else if (onOff == '1') {
-		// on button released create map for buttton pushed or held event based on held time setting
-		log.debug "${device.displayName}: Button released, setting Last Released to current date/time"
+		// on button released create event map for buttton pushed or held and update lastPressed and lastButtonMssg to current date/time
+		displayDebugLog(": Button released, setting lastReleased to current date/time")
 		sendEvent(name: "lastReleased", value: formatDate(), displayed: false)
 		sendEvent(name: "lastReleasedCoRE", value: now(), displayed: false)
 		result = createButtonEvent()
@@ -154,14 +163,15 @@ private createButtonEvent() {
 	def holdTimeMillisec = Math.round((settings.waittoHeld?:2.0) * 1000)
 	def value = "held"
 
-	log.debug "${device.displayName}: Comparing time difference between this button release and last button message"
-	log.debug "${device.displayName}: Time difference = $timeDif ms, Hold time setting = $holdTimeMillisec"
+	displayDebugLog(": Comparing time difference between this button release and last button message")
+	displayDebugLog(": Time difference = $timeDif ms, Hold time setting = $holdTimeMillisec ms")
 	// If there is an issue with message sequence do not parse this button release
 	if (timeDif < 0)
 		return [:]
 	// compare waittoHeld setting with difference between current time and lastButtonMssg
-	else if (timeDif < holdTimeMillisec)
+	else if (timeDif < holdTimeMillisec || timeDif > holdTimeMillisec + 10000)
 		value = "pushed"
+	displayInfoLog(" was ${value}")
 
 	return [
 		name: 'button',
@@ -173,14 +183,14 @@ private createButtonEvent() {
 }
 
 private Map parseReadAttrMessage(String description) {
-
+	def buttonRaw = (description - "read attr - raw:")
 	Map resultMap = [:]
 
 	def cluster = description.split(",").find {it.split(":")[0].trim() == "cluster"}?.split(":")[1].trim()
 	def attrId = description.split(",").find {it.split(":")[0].trim() == "attrId"}?.split(":")[1].trim()
 	def value = description.split(",").find {it.split(":")[0].trim() == "value"}?.split(":")[1].trim()
-	def model = value.split("01FF")[0]
-	def data = value.split("01FF")[1]
+	def model = value.split("02FF")[0]
+	def data = value.split("02FF")[1]
 
 	if (data[4..7] == "0121") {
 		def BatteryVoltage = (Integer.parseInt((data[10..11] + data[8..9]),16))
@@ -195,7 +205,7 @@ private Map parseReadAttrMessage(String description) {
 			def NextChar = (char)Integer.parseInt(str, 16);
 			modelName = modelName + NextChar
 		}
-		log.debug "${device.displayName} reported: cluster: 0000, attrId: 0005, value: ${value}, model:${modelName}, data:${data}"
+		displayDebugLog(" reported: cluster: 0000, attrId: 0005, value: ${value}, model:${modelName}, data:${data}")
 	}
 
 	return resultMap
@@ -205,7 +215,6 @@ private Map parseReadAttrMessage(String description) {
 private Map parseCatchAllMessage(String description) {
 	Map resultMap = [:]
 	def catchall = zigbee.parse(description)
-	log.debug catchall
 
 	if (catchall.clusterId == 0x0000) {
 		def MsgLength = catchall.data.size()
@@ -232,26 +241,37 @@ private Map getBatteryResult(rawValue) {
 	def maxVolts = voltsmax ? voltsmax : 3.0
 	def pct = (rawVolts - minVolts) / (maxVolts - minVolts)
 	def roundedPct = Math.min(100, Math.round(pct * 100))
-	log.debug "${device.displayName}: ${result}"
+	def descText = "Battery at ${roundedPct}% (${rawVolts} Volts)"
+	displayInfoLog(": $descText")
 	return [
 		name: 'battery',
 		value: roundedPct,
 		unit: "%",
 		isStateChange:true,
-		descriptionText : "${device.displayName} Battery at ${roundedPct}% (${rawVolts} Volts)"
+		descriptionText : "$device.displayName $descText"
 	]
+}
+
+private def displayDebugLog(message) {
+	if (debugLogging)
+		log.debug "${device.displayName}${message}"
+}
+
+private def displayInfoLog(message) {
+	if (infoLogging)
+		log.info "${device.displayName}${message}"
 }
 
 //Reset the date displayed in Battery Changed tile to current date
 def resetBatteryRuntime(paired) {
 	def newlyPaired = paired ? " for newly paired sensor" : ""
 	sendEvent(name: "batteryRuntime", value: formatDate(true))
-	log.debug "${device.displayName}: Setting Battery Changed to current date${newlyPaired}"
+	displayInfoLog(": Setting Battery Changed to current date${newlyPaired}")
 }
 
 // installed() runs just after a sensor is paired using the "Add a Thing" method in the SmartThings mobile app
 def installed() {
-	log.debug "${device.displayName}: Installing"
+	displayInfoLog(": Installing")
 	if (!batteryRuntime)
 		resetBatteryRuntime(true)
 	checkIntervalEvent("installed")
@@ -259,7 +279,7 @@ def installed() {
 
 // configure() runs after installed() when a sensor is paired
 def configure() {
-	log.debug "${device.displayName}: Configuring"
+	displayInfoLog(": Configuring")
 	if (!batteryRuntime)
 		resetBatteryRuntime(true)
 	checkIntervalEvent("configured")
@@ -268,16 +288,19 @@ def configure() {
 
 // updated() will run twice every time user presses save in preference settings page
 def updated() {
-	checkIntervalEvent("updated")
-	if(battReset){
+	displayInfoLog(": Updating preference settings")
+	if (battReset){
 		resetBatteryRuntime()
 		device.updateSetting("battReset", false)
 	}
+	checkIntervalEvent("updated")
+	displayInfoLog(": Info message logging enabled")
+	displayDebugLog(": Debug message logging enabled")
 }
 
 private checkIntervalEvent(text) {
 		// Device wakes up every 1 hours, this interval allows us to miss one wakeup notification before marking offline
-		log.debug "${device.displayName}: Configured health checkInterval when ${text}()"
+		displayInfoLog(": Configured health checkInterval when ${text}")
 		sendEvent(name: "checkInterval", value: 2 * 60 * 60 + 2 * 60, displayed: false, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
 }
 
