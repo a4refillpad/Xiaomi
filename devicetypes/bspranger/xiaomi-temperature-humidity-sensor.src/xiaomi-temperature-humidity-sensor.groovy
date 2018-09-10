5e419cf91c92e7acdfbf3b969bd1598bb12ad22a
Keith G <33558908+veeceeoh@users.noreply.github.com>
2018-09-10 11:09:55 -0700

    Change temp colors to standard
    Fix
    • removed an errant ')' that would cause a compile error

--- devicetypes/bspranger/xiaomi-temperature-humidity-sensor.src/xiaomi-temperature-humidity-sensor.groovy
+++ devicetypes/bspranger/xiaomi-temperature-humidity-sensor.src/xiaomi-temperature-humidity-sensor.groovy
@@ -63,14 +63,14 @@ metadata {
             tileAttribute("device.temperature", key:"PRIMARY_CONTROL"){
                 attributeState("temperature", label:'${currentValue}°',
 					backgroundColors:[
- 				[value: 31, color: "#153591"],
- 				[value: 44, color: "#1e9cbb"],
- 				[value: 59, color: "#90d2a7"],
- 				[value: 74, color: "#44b621"],
- 				[value: 84, color: "#f1d801"],
- 				[value: 95, color: "#d04e00"],
- 				[value: 96, color: "#bc2323"]
- 				]
+ 						[value: 31, color: "#153591"],
+ 						[value: 44, color: "#1e9cbb"],
+ 						[value: 59, color: "#90d2a7"],
+ 						[value: 74, color: "#44b621"],
+ 						[value: 84, color: "#f1d801"],
+ 						[value: 95, color: "#d04e00"],
+ 						[value: 96, color: "#bc2323"]
+ 					]
                 )
             }
             tileAttribute("device.multiAttributesReport", key: "SECONDARY_CONTROL") {
@@ -79,17 +79,16 @@ metadata {
             }
         }
         valueTile("temperature2", "device.temperature", inactiveLabel: false) {
-            state "temperature", label:'${currentValue}°', icon: "st.Weather.weather2",
-                backgroundColors:[
- 				[value: 31, color: "#153591"],
- 				[value: 44, color: "#1e9cbb"],
- 				[value: 59, color: "#90d2a7"],
- 				[value: 74, color: "#44b621"],
- 				[value: 84, color: "#f1d801"],
- 				[value: 95, color: "#d04e00"],
- 				[value: 96, color: "#bc2323"]
- 				]
-                )
+        	state "temperature", label:'${currentValue}°', icon: "st.Weather.weather2",
+        		backgroundColors:[
+ 	    			[value: 31, color: "#153591"],
+ 	    			[value: 44, color: "#1e9cbb"],
+ 	    			[value: 59, color: "#90d2a7"],
+ 	    			[value: 74, color: "#44b621"],
+ 	    			[value: 84, color: "#f1d801"],
+ 	    			[value: 95, color: "#d04e00"],
+ 	    			[value: 96, color: "#bc2323"]
+ 	    		]
             }
         valueTile("humidity", "device.humidity", inactiveLabel: false, width: 2, height: 2) {
             state "humidity", label:'${currentValue}%', unit:"%", icon:"https://raw.githubusercontent.com/bspranger/Xiaomi/master/images/XiaomiHumidity.png",
